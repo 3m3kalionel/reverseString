852f9b9915b939ddf059acc01d34f138402d5463
3m3ka <emeka.l.okoro@gmail.com>
2017-05-11 14:38:41 +0100

    add the tests

---
+++ spec/tests.spec.js
@@ -0,0 +1,60 @@
+const reverseString = require('../app/app.js').reverseString;
+
+describe("Produce the reverse order of a word: ", function() {
+  describe("Case for en empty string", function() {
+
+    it("should return null for empty string", function() {
+      expect(reverseString('')).toEqual(null);
+    });
+
+  });
+
+  describe("Case for palindromes", function() {
+
+    it("should return true for `anna`", function() {
+      expect(reverseString('anna')).toEqual(true);
+    });
+
+    it("should return true for `NaN`", function() {
+      expect(reverseString('NaN')).toEqual(true);
+    });
+
+    it("should return true for `civic`", function() {
+      expect(reverseString('civic')).toEqual(true);
+    });
+
+  });
+
+  describe("Case for normal words", function() {
+
+    it("should return `skoob` for `books`", function() {
+      expect(reverseString('books')).toEqual('skoob');
+    });
+
+    it("should return `nomolos` for `solomon`", function() {
+      expect(reverseString('solomon')).toEqual('nomolos');
+    });
+
+    it("should return `csim` for `misc`", function() {
+      expect(reverseString('misc')).toEqual('csim');
+    });
+
+  });
+
+  describe("Case for palindromes", function() {
+
+    it("should return true for `abiriba`", function() {
+      expect(reverseString('anna')).toEqual(true);
+    });
+
+    it("should return true for `radar`", function() {
+      expect(reverseString('radar')).toEqual(true);
+    });
+
+    it("should return true for `civic`", function() {
+      expect(reverseString('level')).toEqual(true);
+    });
+
+  });
+
+});
